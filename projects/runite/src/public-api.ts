207c197f2881e4df10810e18fff85b4846848670
Tynarus <github@tyn.dev>
2021-02-27 17:47:15 -0600

    Updating public api

--- projects/runite/src/public-api.ts
+++ projects/runite/src/public-api.ts
@@ -1,3 +1,4 @@
+
 /**
  * Runite Root Module
  */
@@ -8,13 +9,35 @@ export * from './lib/runite.module';
  * Control Components
  */
 export * from './lib/controls/controls.module';
+export * from './lib/controls/button/button.module';
+export * from './lib/controls/button/button.component';
+
 
 /**
  * Design Components
  */
 export * from './lib/design/design.module';
+export * from './lib/design/footer/footer.module';
+export * from './lib/design/footer/footer.component';
+export * from './lib/design/header/header.module';
+export * from './lib/design/header/header.component';
+export * from './lib/design/panel/panel.module';
+export * from './lib/design/panel/panel.component';
+export * from './lib/design/scroll/scroll.module';
+export * from './lib/design/scroll/scroll.component';
+export * from './lib/design/table/table.module';
+export * from './lib/design/table/table.component';
+
 
 /**
  * Layout Components
  */
 export * from './lib/layout/layout.module';
+export * from './lib/layout/scroll-layout/scroll-layout.module';
+export * from './lib/layout/scroll-layout/scroll-layout.component';
+
+
+/**
+ * Misc
+ */
+export * from './lib/colors';
